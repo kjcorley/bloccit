88343eb9cb5ad2d06f747792761d4ed68443fcb7
Kevin Corley <kjcorley@gmail.com>
2018-10-11 14:23:55 -0400

    added log for environment port

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const app = require("./app");
 const http = require("http");
 
 const port = normalizePort(process.env.PORT || "3000");
+console.log(process.env.PORT);
 app.set("port", port);
 
 const server = http.createServer(app);
